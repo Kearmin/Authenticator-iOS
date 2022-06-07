ebb37fb9ba02bdc7a781568ccbd5b55f38b14f50
Kertész Jenő Ármin <kertesz.armin96@gmail.com>
2022-06-07 02:31:48 +0200

    weakref tests

--- Authenticator-iOS/Authenticator-iOSIntegrationTests/AuthenticatorListIntegrationTests.swift
+++ Authenticator-iOS/Authenticator-iOSIntegrationTests/AuthenticatorListIntegrationTests.swift
@@ -8,21 +8,57 @@
 import XCTest
 import Combine
 import AuthenticatorListBusiness
+import AuthenticatorListView
 @testable import Authenticator_iOS
 
 class AuthenticatorListIntegrationTests: XCTestCase {
     func test_ListHastTitle() {
-        let loader = ListLoaderStub()
-        let viewController = ListComposer.list(dependencies: .init(
-            totpProvider: TOTPProviderMock(),
-            readAccounts: loader.readAccounts,
-            delete: loader.delete,
-            favourite: loader.favourite,
-            update: loader.update,
-            refreshPublisher: loader.refresh,
-            clockPublisher: loader.clock)
-        )
-        XCTAssertEqual(viewController.0.title, "Authenticator")
+        let env = makeSUT()
+        XCTAssertEqual(env.sutViewController.title, "Authenticator")
+    }
+
+    func test_ListCanRenderAccounts() {
+    }
+
+    func makeSUT() -> TestEnvironment {
+        let env = TestEnvironment()
+        trackForMemoryLeaks(env.sutViewController)
+        trackForMemoryLeaks(env.sutViewModel)
+        trackForMemoryLeaks(env.loader)
+        return env
+    }
+
+    class TestEnvironment {
+        let loader: ListLoaderStub
+        let sutView: AuthenticatorListView
+        let sutViewController: AuthenticatorListViewController
+        let sutViewModel: AuthenticatorListViewModel
+
+        init(loader: ListLoaderStub = .init()) {
+            self.loader = loader
+            self.sutViewController = ListComposer.list(dependencies: .init(
+                totpProvider: TOTPProviderMock(),
+                readAccounts: loader.readAccounts,
+                delete: loader.delete,
+                favourite: loader.favourite,
+                update: loader.update,
+                refreshPublisher: loader.refresh,
+                clockPublisher: loader.clock)
+            ).0
+            self.sutView = sutViewController.rootView
+            self.sutViewModel = sutViewController.viewModel
+        }
+    }
+}
+
+extension AuthenticatorListIntegrationTests {
+}
+
+extension XCTestCase {
+    func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
     }
 }
 
